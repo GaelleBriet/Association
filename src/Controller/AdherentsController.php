c7c22fb50a3be8eeb88e2d889d17d61eff0b49a4
GaelleBriet <gaelle.briet@protonmail.com>
2022-09-05 16:23:01 +0200

    Add show page for adherents

--- src/Controller/AdherentsController.php
+++ src/Controller/AdherentsController.php
@@ -2,37 +2,83 @@
 
 namespace App\Controller;
 
+use App\Entity\Adherents;
 use App\Repository\AdherentsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/adherents')]
 class AdherentsController extends AbstractController
 {
-    #[Route('', name: 'app_adherents')]
-    #[Route('/list', name: 'app_adherents_list')]
+    #[Route('', name: 'app_adherents', methods: 'GET')]
+    #[Route('/list', name: 'app_adherents_list', methods: 'GET')]
     public function list(AdherentsRepository $adherentsRepository): Response
     {
         $adherents = $adherentsRepository->findAll();
         return $this->render('adherents/list.html.twig', compact('adherents'));
     }
 
-    #[Route('/show', name: 'app_adherents_show')]
-    public function show(): Response
+    #[Route('/{id<[0-9]+>}', name: 'app_adherents_show', methods: 'GET')]
+    public function show(Adherents $adherent): Response
     {
-        return $this->render('adherents/show.html.twig');
+        return $this->render('adherents/show.html.twig', [
+            'adherent' => $adherent
+        ]);
     }
 
-    #[Route('/edit', name: 'app_adherents_edit')]
-    public function edit(): Response
+    #[Route('/{id<[0-9]+>/edit}', name: 'app_adherents_edit')]
+    public function edit(Adherents $adherent, Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('adherents/edit.html.twig');
+        $form = $this->createFormBuilder($adherent)
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('tel', TelType::class)
+            ->add('email', EmailType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('app_adherents');
+        }
+
+        return $this->render('adherents/edit.html.twig',  [
+            'adherents' => $adherent,
+            'form' => $form->createView()
+        ]);
     }
 
-    #[Route('/add', name: 'app_adherents_add')]
-    public function add(): Response
+    #[Route('/create', name: 'app_adherents_create', methods: 'GET|POST')]
+    public function create(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('adherents/edit.html.twig');
+        $adherent = new Adherents;
+
+        $form = $this->createFormBuilder($adherent)
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('tel', TelType::class)
+            ->add('email', EmailType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($adherent);
+            $em->flush();
+
+            return $this->redirectToRoute('app_adherents');
+        }
+
+        return $this->render('adherents/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
--- src/Repository/AdherentsRepository.php
+++ src/Repository/AdherentsRepository.php
@@ -39,20 +39,19 @@ class AdherentsRepository extends ServiceEntityRepository
         }
     }
 
-    //    /**
-    //     * @return Adherents[] Returns an array of Adherents objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('a.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    // /**
+    //  * @return Adherents[] Returns an array of Adherents objects
+    //  */
+    // public function findByDate($value): array
+    // {
+    //     return $this->createQueryBuilder('a')
+    //         ->andWhere('a.exampleField = :val')
+    //         ->setParameter('val', $value)
+    //         ->orderBy('a.id', 'ASC')
+    //         ->setMaxResults(10)
+    //         ->getQuery()
+    //         ->getResult();
+    // }
 
     //    public function findOneBySomeField($value): ?Adherents
     //    {
